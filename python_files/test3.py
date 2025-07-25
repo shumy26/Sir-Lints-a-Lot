5f64697c0853426b53f4d2c31442329e2baf33f0
shumy26 <231012826@aluno.unb.br>
2025-07-25 17:51:24 -0300

    test python files

---
+++ python_files/test3.py
@@ -0,0 +1,12 @@
+number1 = 5
+number2 = 2
+#number3 = number1 + number2
+
+if number1 > 3:
+ print(number1)
+
+else:
+    print(number2) #comment!
+    for i in range(3): # comment as well
+       print("a") #  yet another comment
+       
