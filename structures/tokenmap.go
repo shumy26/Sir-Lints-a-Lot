39128faeb9433f2ffcad7925f59d64396462f119
shumy26 <231012826@aluno.unb.br>
2025-07-25 13:57:49 -0300

    global token map

---
+++ structures/tokenmap.go
@@ -0,0 +1,18 @@
+package structures
+
+import "fmt"
+
+type GlobalTokenMap struct {
+	TokenMap           map[string]Token // key is token name
+	TokensWithProblems map[string]Token
+}
+
+func (t *GlobalTokenMap) AddToken(token Token) error {
+	if _, ok := t.TokenMap[token.Name]; ok {
+		return fmt.Errorf("token with this name already exists")
+	} else if token.NumOccurences <= 1 {
+		t.TokenMap[token.Name] = token
+		t.TokensWithProblems[token.Name] = token
+	}
+	return nil
+}
