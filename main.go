e764d23cf4360a0488b2ee0f465929f6025ebf3c
agialias <agialias4000@gmail.com>
2025-07-26 00:25:44 +0100

    Testing CreateBlock and CreateTokens.

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"os"
 	"strings"
+
+	"github.com/shumy26/Sir-Lints-a-Lot/structures"
 )
 
 //var globalTokenMap = &structures.GlobalTokenMap{
@@ -14,17 +16,29 @@ import (
 
 func main() {
 	if len(os.Args) < 2 {
-		log.Fatal("No file found")
+		log.Fatal("No filepath provided")
 	}
+
 	path := os.Args[1]
 	if !strings.HasSuffix(path, ".py") {
-		fmt.Println("Not a Python file")
-		os.Exit(1)
+		log.Fatalf("Not a Python file")
 	}
+
 	fileBytes, err := os.ReadFile(path)
 	if err != nil {
 		log.Fatalf("Error reading file: %v", err)
 	}
 	fileText := string(fileBytes)
-	fmt.Println(fileText)
+
+	block := structures.CreateBlock(fileText, path, 0, 100)
+	block.CreateTokens()
+
+	for i := 0; i < len(block.TokenList); i++ {
+		fmt.Println(" ")
+		fmt.Printf("Name:\t%v\n", block.TokenList[i].Name)
+		fmt.Printf("Occurances:\t%v\n", block.TokenList[i].NumOccurences)
+		fmt.Printf("Line Number:\t%v\n", block.TokenList[i].LocationLine)
+		fmt.Printf("File:\t%v\n", block.TokenList[i].LocationFile)
+		fmt.Println(" ")
+	}
 }
