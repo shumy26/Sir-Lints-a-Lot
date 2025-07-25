4e25f5b80c059a016f5b3e002758ac9ea9538dca
Gabriel G. R. Silva <gabrielgrsilva@outlook.com.br>
2025-07-25 16:19:34 -0300

    Fix bugs in CreateTokens() and blockWordMaps()

--- structures/blocks.go
+++ structures/blocks.go
@@ -1,5 +1,9 @@
 package structures
 
+import (
+	"strings"
+)
+
 type Block struct {
 	Code              string
 	LocationFile      string
@@ -7,7 +11,6 @@ type Block struct {
 	LocationLineEnd   int
 	TokenList         []Token
 }
-
 
 func CreateBlock(code, file string, lineStart, lineEnd int) Block {
 	block := Block{
@@ -19,59 +22,61 @@ func CreateBlock(code, file string, lineStart, lineEnd int) Block {
 	return block
 }
 
-func (b *Block) CreateTokens() []Token, error { //DONT FORGET TO IMPORT "strings"
+func (b *Block) CreateTokens() ([]Token, error) {
 
-	wordList, numOccurences := blockWordMaps(b *Block)
-	
-	for i := 0; i < len(wordList); i++ {
+	var tokenList []Token
 
-		token := Token{
-			Name:          wordList[i]
-			NumOccurences: numOccurences[i]
-			LocationFile  []string
-			LocationLine  []int
+	wordCount, wordLines := b.blockWordMaps()
 
+	for name, occurences := range wordCount { //Iterate over the map
+		for wordname, wordLines := range wordLines {
+			if name == wordname {
+				linesOfWord := wordLines
+
+				token := Token{
+					Name:          name,
+					NumOccurences: occurences,
+					LocationFile:  b.LocationFile, // Block has only string here, not []string
+					LocationLine:  linesOfWord,
+				}
+
+				tokenList = append(tokenList, token)
+			}
 		}
 
 	}
 
-	return nil
+	return tokenList, nil
 }
 
-func blockWordMaps(b *Block) map[string]int,  { //Helper function for CreateTokens()
+func (b *Block) blockWordMaps() (map[string]int, map[string][]int) { //Helper function for CreateTokens()
 
-	wordCount := make(map[string]int) 	 	// Map to store words and their counts (word -> count)
-	wordLineCount := make(map[string][]int)  // Map to store on which lines a word appears (word -> lines)
-    wordLineOccur := make(map[string][]int)   // Map to store num of occurences per line of a word (word -> occurences)
+	wordCount := make(map[string]int)   // Map to store words and their counts (word -> count)
+	wordLines := make(map[string][]int) // Map to store on which lines a word appears (word -> lines)
 
-    lines := strings.Split(b.Code, "\n") //Splits each line of the code
+	lines := strings.Split(b.Code, "\n") //Splits each line of the code
 
- 	for lineNum, line := range lines { //Iterates over each line
-        words := strings.Fields(line)
-        lineWordCount := make(map[string]int)
+	for lineNum, line := range lines { //Iterates over each line
+		words := strings.Fields(line)
 
-        for _, w := range words {
-            wordCount[w]++ //Increase global word count
-            lineWordCount[w]++ //Word count JUST for that particular line
-        }
+		for _, w := range words {
+			wordCount[w]++                                 //Increase global word count
+			wordLines[w] = append(wordLines[w], lineNum+1) // +1 because we don't have line 0 in most IDEs
+		}
+	}
 
-        for w, count := range lineWordCount {
-            wordLineCount[w] = append(wordLineCount[w], lineNum+1) // +1 because we don't have line 0 in most IDEs
-            wordLineOccur[w] = append(wordLineOccur[w], count)
-        }
-    }
+	return wordCount, wordLines
 
-    return wordCount, wordLineCount, wordLineOccur
 }
 
 func countLeadingWhitespace(line string) int { //Helper function to count the leading whitespace on each line of the code
-    count := 0
-    for _, ch := range line {
-        if ch == ' ' || ch == '\t' { //Separates by leading whitespace or "tabs"
-            count++
-        } else {
-            break
-        }
-    }
-    return count
+	count := 0
+	for _, ch := range line {
+		if ch == ' ' || ch == '\t' { //Separates by leading whitespace or "tabs"
+			count++
+		} else {
+			break
+		}
+	}
+	return count
 }
