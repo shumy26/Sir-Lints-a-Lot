ac6fb6c06e00d28a2ba8fc429a883110d1a00c5c
shumy26 <231012826@aluno.unb.br>
2025-07-25 23:04:12 -0300

    fix typo

--- structures/tokens.go
+++ structures/tokens.go
@@ -3,17 +3,17 @@ package structures
 import "fmt"
 
 type Token struct {
-	Name          string
-	NumOccurences int
-	LocationFile  string
-	LocationLine  []int
+	Name           string
+	NumOccurrences int
+	LocationFile   string
+	LocationLine   []int
 }
 
 func (t Token) PrintToken() error {
-	if len(t.LocationFile) != len(t.LocationLine) || len(t.LocationFile) != t.NumOccurences {
+	if len(t.LocationFile) != len(t.LocationLine) || len(t.LocationFile) != t.NumOccurrences {
 		return fmt.Errorf("Token has inconsistent number of occurences")
 	}
-	fmt.Printf("Token %s with %d occurences at locations:\n", t.Name, t.NumOccurences)
+	fmt.Printf("Token %s with %d occurences at locations:\n", t.Name, t.NumOccurrences)
 	for i := range t.LocationFile {
 		fmt.Printf("%s at line %d\n", t.LocationFile, t.LocationLine[i])
 	}
@@ -22,14 +22,14 @@ func (t Token) PrintToken() error {
 
 func (t *Token) AddOccurence(file string, line int) {
 	t.LocationLine = append(t.LocationLine, line)
-	t.NumOccurences++
+	t.NumOccurrences++
 }
 
 func NewToken(name, file string, numOccurences int, lines []int) Token {
 	return Token{
-		Name:          name,
-		LocationFile:  file,
-		NumOccurences: numOccurences,
-		LocationLine:  lines,
+		Name:           name,
+		LocationFile:   file,
+		NumOccurrences: numOccurences,
+		LocationLine:   lines,
 	}
 }
