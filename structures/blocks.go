e9da8ee44f9cb1ab18352be46eae5cffc304e287
Gabriel G. R. Silva <gabrielgrsilva@outlook.com.br>
2025-07-27 18:32:50 -0300

    Fix bug in regex betweenQuotes - it now selects stuff between ()

--- structures/blocks.go
+++ structures/blocks.go
@@ -46,7 +46,7 @@ func (b *Block) CreateTokens() []Token { //Receives a Block and returns a slice 
 func (b *Block) blockWordMaps() (map[string]int, map[string][]int) { //Helper function for CreateTokens()
 
 	var genRe = regexp.MustCompile(`[A-Za-z_][A-Za-z_0-9]*(\.[A-Za-z_][A-Za-z_0-9]*)*|[^\sA-Za-z_0-9.]`) //General regex to separate words and symbols WITHOUT whitespaces, maintaining dots
-	var betweenQuotes = regexp.MustCompile(`"[^"]*"`)                                                    //Selects strings - stuff between quotes "" - so we may ignore them
+	var betweenQuotes = regexp.MustCompile(`"[^"]*"|\([^)]*\)`)                                          //Selects strings - stuff between quotes "" - so we may ignore them
 
 	pythonKeywords := [35]string{"False", "await", "else", "import", "pass", "None", "break", "except", "in", "raise", "True", "class", "finally", "is", "return", "and", "continue", "for", "lambda", "try", "as", "def", "from", "nonlocal", "while", "assert", "del", "global", "not", "with", "async", "elif", "if", "or", "yield"}
 	pythonOps := [41]string{"'", "(", ")", "{", "}", "[", "]", "+", "-", "*", "/", "//", "%", "**", "==", "!=", ">", "<", ">=", "<=", "=", "+=", "-=", "*=", "/=", "//=", "%=", "**=", "&=", "|=", "^=", ">>=", "<<=", "&", "|", "^", "~", "<<", ">>"}
